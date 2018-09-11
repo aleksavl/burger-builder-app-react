20b93d9d67007d3cd6fe1dadfd689875ff47081f
aleksavl <aleksavl@gmail.com>
2018-09-11 08:38:26 +0200

    Navigation completed

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,36 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-  .map((igKey,i) => {
+class OrderSummary extends Component {
+  //This could be functional component
+  render () {
+    const ingredientSummary = Object.keys(this.props.ingredients)
+    .map((igKey,i) => {
+      return (
+        <li key={igKey}>
+          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
+        </li>
+      )
+    });
     return (
-      <li key={igKey}>
-        <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
-      </li>
-    )
-  });
-  return (
-    <React.Fragment>
-      <h3>Your Order</h3>
-      <p>Delicious burger with following ingredients:</p>
-      <ul>
-        {ingredientSummary}
-      </ul>
-      <p><strong>Total price: {props.price.toFixed(2)} $</strong></p>
-      <p>Continue to checkout?</p>
-      <Button
-        btnType="Danger"
-        clicked={props.purchaseCanceled}>
-        Cancel
-      </Button>
-      <Button
-        btnType="Success"
-        clicked={props.purchaseContinued}>
-        Continue
-      </Button>
-    </React.Fragment>
-  );
-};
+      <React.Fragment>
+        <h3>Your Order</h3>
+        <p>Delicious burger with following ingredients:</p>
+        <ul>
+          {ingredientSummary}
+        </ul>
+        <p><strong>Total price: {this.props.price.toFixed(2)} $</strong></p>
+        <p>Continue to checkout?</p>
+        <Button
+          btnType="Danger"
+          clicked={this.props.purchaseCanceled}>
+          Cancel
+        </Button>
+        <Button
+          btnType="Success"
+          clicked={this.props.purchaseContinued}>
+          Continue
+        </Button>
+      </React.Fragment>
+    );
+  }
+}
 
-export default orderSummary;
+export default OrderSummary;
